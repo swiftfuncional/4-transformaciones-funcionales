c6d61b0fc93929b7cc86bb2093c7b5478e6c286b
Swift Funcional <info@swiftfuncional.com>
2017-03-27 18:17:27 +0200

    Extract hosts list without repeating

--- Transformaciones funcionales.playground/Contents.swift
+++ Transformaciones funcionales.playground/Contents.swift
@@ -3,3 +3,14 @@ var userDatabase = JsonArray()
 for db in UserConsumer.databases {
 	userDatabase.append(contentsOf: db)
 }
+
+var hosts = [String]()
+
+for user in userDatabase {
+	if let email = user["email"] as? String,
+		let host = email.components(separatedBy: "@").last,
+		!hosts.contains(host) {
+
+		hosts.append(host)
+	}
+}
