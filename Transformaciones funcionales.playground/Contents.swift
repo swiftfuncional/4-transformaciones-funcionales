e9b736911eea5a71f1c4e9146122c68ca20ca4e3
Swift Funcional <info@swiftfuncional.com>
2017-03-27 18:38:27 +0200

    Force unwrap hosts

--- Transformaciones funcionales.playground/Contents.swift
+++ Transformaciones funcionales.playground/Contents.swift
@@ -4,10 +4,11 @@ for db in UserConsumer.databases {
 	userDatabase.append(contentsOf: db)
 }
 
-let hosts: [String?] = userDatabase
+let hosts: [String] = userDatabase
 	.map { ($0["email"] as? String)?.components(separatedBy: "@").last }
 	.filter { (optionalHost) -> Bool in
-		optionalHost != nil }
+		optionalHost != nil
+	}.map { $0! }
 
 typealias  HostInfo = (count: Int, age: Int)
 
