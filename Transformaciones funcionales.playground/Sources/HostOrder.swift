8086fff227988d564364f95348110d8d2922744b
Swift Funcional <info@swiftfuncional.com>
2017-04-04 09:28:34 +0200

    Complete sort method

--- Transformaciones funcionales.playground/Sources/HostOrder.swift
+++ Transformaciones funcionales.playground/Sources/HostOrder.swift
@@ -6,5 +6,13 @@ public enum HostOrder {
 	public typealias Order = (Host, Host) -> Bool
 
 	public var sort: Order {
+		switch self {
+		case .Age:
+			return { $0.info.age < $1.info.age }
+		case .Count:
+			return { $0.info.count < $1.info.count }
+		case .Name:
+			return { $0.host < $1.host }
+		}
 	}
 }
